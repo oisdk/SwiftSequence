8a5cea2f0a7d9548896108aa9d6b54a5e5cf77f8
oisdk <oisinkidney@Oisins-MacBook-Air.local>
2015-06-29 22:05:58 +0100

    added @noescape parameter

--- SwiftSequence/Finding.swift
+++ SwiftSequence/Finding.swift
@@ -3,7 +3,7 @@ public extension SequenceType {
   /// Returns the first element in self that satisfies a predicate, or nil if it doesn't
   /// exist
   
-  func first(predicate: Generator.Element -> Bool) -> Generator.Element? {
+  func first(@noescape predicate: Generator.Element -> Bool) -> Generator.Element? {
     for el in self where predicate(el) { return el }
     return nil
   }
@@ -15,7 +15,7 @@ public extension CollectionType {
   /// Returns the last element in self that satisfies a predicate, or nil if it doesn't
   /// exist
   
-  func last(predicate: Generator.Element -> Bool) -> Generator.Element? {
+  func last(@noescape predicate: Generator.Element -> Bool) -> Generator.Element? {
     for el in lazy(self).reverse() where predicate(el) { return el }
     return nil
   }

0d62ce994eab71e09f0456e374a357587056e3fe
oisdk <oisinkidney@oisins-macbook-air.local>
2015-08-03 17:08:49 +0100

    Put check() method in initialiser

--- SwiftSequence/Deque.swift
+++ SwiftSequence/Deque.swift
@@ -3,6 +3,16 @@
 public struct Deque<Element> {
   private var front: List<Element> { didSet { check() } }
   private var back : List<Element> { didSet { check() } }
+  private init(_ front: List<Element>, _ back: List<Element>) {
+    (self.front, self.back) = (front, back)
+    check()
+  }
+}
+
+extension Deque {
+  private init(balancedFront: List<Element>, balancedBack: List<Element>) {
+    (front, back) = (balancedFront, balancedBack)
+  }
 }
 
 extension Deque : CustomDebugStringConvertible {
@@ -97,20 +107,16 @@ extension Deque {
 
 extension Deque {
   public func reverse() -> Deque<Element> {
-    return Deque(front: back, back: front)
+    return Deque(balancedFront: back, balancedBack: front)
   }
 }
 
 extension Deque {
   public var tail: Deque<Element> {
-    var ret = Deque(front: front.tail, back: back)
-    ret.check()
-    return ret
+    return Deque(front.tail, back)
   }
   public var initial: Deque<Element> {
-    var ret = Deque(front: front, back: back.tail)
-    ret.check()
-    return ret
+    return Deque(front, back.tail)
   }
 }
 
@@ -134,39 +140,27 @@ extension Deque {
 extension Deque {
   public func map<T>(@noescape transform: Element -> T) -> Deque<T> {
     return Deque<T>(
-      front: front.map(transform),
-      back : back .map(transform)
+      balancedFront: front.map(transform),
+      balancedBack : back .map(transform)
     )
   }
 }
 
 extension Deque {
   public func filter(@noescape includeElement: Element -> Bool) -> Deque<Element> {
-    var ret = Deque(
-      front: front.filter(includeElement),
-      back : back .filter(includeElement)
-    )
-    ret.check()
-    return ret
+    return Deque(front.filter(includeElement), back .filter(includeElement))
   }
 }
 
 extension Deque {
   public func flatMap<T>(@noescape transform: Element -> Deque<T>) -> Deque<T> {
-    var ret = Deque<T>(
-      front: front.flatMap{List(transform($0))},
-      back : back .flatMap{List(transform($0).reverse())}
+    return Deque<T>(
+      front.flatMap{List(transform($0))},
+      back .flatMap{List(transform($0).reverse())}
     )
-    ret.check()
-    return ret
   }
   
   public func flatMap<T>(@noescape transform: Element -> T?) -> Deque<T> {
-    var ret = Deque<T>(
-      front: front.flatMap(transform),
-      back : back .flatMap(transform)
-    )
-    ret.check()
-    return ret
+    return Deque<T>(front.flatMap(transform), back.flatMap(transform))
   }
 }

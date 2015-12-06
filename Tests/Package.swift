2cdf01c9699bf18bfbe2d96b9f06b574f2b09eab
oisdk <oisin.kidney@gmail.com>
2015-12-06 00:07:56 +0000

    url typo

--- Tests/Package.swift
+++ Tests/Package.swift
@@ -1,14 +1,9 @@
 import PackageDescription
 
 let package = Package(
-<<<<<<< HEAD
-  name: "SwiftSequenceTests"
-  dependencies
-=======
   name: "SwiftSequenceTests",
   dependencies: [
-    .Package(url: "https://github.com/oisdk/SwiftSequence/tree/Package.git", majorVersion: 1),
-    .Package(url: "https://github.com/apple/swift-corelibs-xctest", majorVersion: 1)
+    .Package(url: "https://github.com/oisdk/SwiftSequence.git", majorVersion: 1),
+    .Package(url: "https://github.com/apple/swift-corelibs-xctest.git", majorVersion: 1)
   ]
->>>>>>> origin/master
 )

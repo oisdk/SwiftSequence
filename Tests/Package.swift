2626f87d09d7c0076e91cf927681a2767090ddf8
oisdk <oisin.kidney@gmail.com>
2015-12-06 00:03:01 +0000

    some tests

--- Tests/Package.swift
+++ Tests/Package.swift
@@ -1,6 +1,9 @@
 import PackageDescription
 
 let package = Package(
-  name: "SwiftSequenceTests"
-  dependencies
+  name: "SwiftSequenceTests",
+  dependencies: [
+    .Package(url: "https://github.com/oisdk/SwiftSequence/tree/Package.git", majorVersion: 1),
+    .Package(url: "https://github.com/apple/swift-corelibs-xctest", majorVersion: 1)
+  ]
 )
